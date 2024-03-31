d7c0b05c081223adcdaed69d30122c425520b8cc
eveeifyeve <eveeg1971@gmail.com>
2024-04-01 07:29:56 +1100

    chore: removed raycast and unused varables

--- .config/nix-darwin/configuration.nix
+++ .config/nix-darwin/configuration.nix
@@ -1,11 +1,9 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
 # Nix-Darwin Config
 services.nix-daemon.enable = true;
 users.users.eveeifyeve.home = "/Users/eveeifyeve";
   nix.settings.experimental-features = "nix-command flakes";
   nix.settings.allowed-users = [ "eveeifyeve" "root" ];
   nixpkgs.config.allowUnfree = true;
-  system.configurationRevision = self.rev or self.dirtyRev or null;
   nixpkgs.hostPlatform = "aarch64-darwin";
-  environment.systemPackages = with pkgs; [ raycast ];
 }
