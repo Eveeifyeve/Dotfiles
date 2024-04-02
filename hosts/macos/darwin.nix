c0747524803db52590096463b17921c19af8c1b5
eveeifyeve <eveeg1971@gmail.com>
2024-04-03 06:58:48 +1100

    feat: removed zsh enable

--- hosts/macos/darwin.nix
+++ hosts/macos/darwin.nix
@@ -4,6 +4,5 @@ nix.settings.allowed-users = [ "eveeifyeve" "root" ];
 nixpkgs.hostPlatform = "aarch64-darwin";
  nixpkgs.config.allowUnfree = true;
 services.nix-daemon.enable = true;
-programs.zsh.enable = true;
 system.stateVersion = 4;
 }
