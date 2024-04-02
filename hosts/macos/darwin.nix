9a6940ea122c5851ab76220988f816607639b9d3
eveeifyeve <eveeg1971@gmail.com>
2024-04-03 04:25:31 +1100

    chore: removed homebrew install and rely on nix

--- hosts/macos/darwin.nix
+++ hosts/macos/darwin.nix
@@ -1,13 +1,4 @@
 {config, pkgs, ...}: {
-# Nix-Darwin Config
-  homebrew = {
-    enable = true;
-    casks = [
-      "spotify"
-      "vscode"
-    ];
-  };
-
 nix.settings.experimental-features = "nix-command flakes";
 nix.settings.allowed-users = [ "eveeifyeve" "root" ];
 nixpkgs.hostPlatform = "aarch64-darwin";
