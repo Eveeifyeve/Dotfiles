492d849bdddc0938f9f5d1ed84dbe1af124da216
eveeifyeve <eveeg1971@gmail.com>
2024-04-17 05:06:23 +1000

    fix: tauri flake template

--- flakes/tauri/flake.nix
+++ flakes/tauri/flake.nix
@@ -47,26 +47,26 @@
             imports = [ ];
 
             # https://devenv.sh/reference/options/
-            packages =
-              lib.optionals pkgs.stdenv.isDarwin (
-                with pkgs.darwin.apple_sdk.frameworks;
-                [
-                  Security
-                  SystemConfiguration
-                  AppKit
-                  WebKit
-                  # Add other Darwin-specific packages here
-                ]
-              )
-              ++ (with pkgs; [
-                llvmPackages.libcxxStdenv
-                darwin.libobjc
-                rustup
-              ]);
+packages =
+ lib.optionals pkgs.stdenv.isDarwin (
+    with pkgs.darwin.apple_sdk.frameworks;
+    [
+      Security
+      SystemConfiguration
+      AppKit
+      WebKit
+      # Add other Darwin-specific packages here
+    ]
+ )
+ ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+    llvmPackages.libcxxStdenv
+    llvmPackages.libcxxClang
+    darwin.libobjc
+    rustup
+ ]);
 
             # Define Enviroment Virables
             env = {
-              CC="clang -fobjc-exceptions";
             };
 
             # https://devenv.sh/scripts/
@@ -91,10 +91,11 @@
 
             languages.javascript = {
               enable = true;
-              bun = {
-                enable = true;
-                install.enable = true;
-              };
+              # Enable your Favourite Package Manager via here.
+              # bun = {
+              #   enable = true;
+              #   install.enable = true;
+              # };
             };
 
             languages.typescript = {
