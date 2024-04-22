f107ed7e7165afa070072c95b576236b7fe707bd
eveeifyeve <eveeg1971@gmail.com>
2024-04-23 09:41:19 +1000

    feat: neovim plugins

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,3 +1,8 @@
-{
-  
+{pkgs, ...}: {
+  programs.nixvim = {
+    enable = true;
+    extraPlugins = with pkgs; [ vimPlugins.lazy-nvim ];
+    colorschemes.gruvbox.enable = true;
+    plugins.lightline.enable = true;
+  };
 }
