c9b230f9e4f42c3bc2b10060340911135b69c5a7
eveeifyeve <eveeg1971@gmail.com>
2024-04-23 17:08:13 +1000

    chore: improved extra plugins

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.nixvim = {
     enable = true;
-    extraPlugins = with pkgs; [ vimPlugins.lazy-nvim ];
+    extraPlugins = with pkgs.vimPlugins; [ lazy-nvim ];
     colorschemes.gruvbox.enable = true;
     plugins.lightline.enable = true;
   };
