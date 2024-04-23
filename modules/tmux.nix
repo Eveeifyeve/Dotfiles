cd2ee48c2abfebb50448047b4b7c7069ef5be08b
eveeifyeve <eveeg1971@gmail.com>
2024-04-24 00:13:13 +1000

    feat: tmux theme

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,6 +1,10 @@
-{
+{pkgs, ...}: {
  programs.tmux = {
     enable = true;
+     plugins = with pkgs;
+      [
+         tmuxPlugins.catppuccin
+      ];
     extraConfig = builtins.readFile ../tmux.conf;
  };
 }
