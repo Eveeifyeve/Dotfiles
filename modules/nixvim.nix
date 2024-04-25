a9b81cc822d89299ba5b72130205df6ecf200a63
eveeifyeve <eveeg1971@gmail.com>
2024-04-25 18:14:52 +1000

    feat: telescope for nav

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -3,6 +3,7 @@
     enable = true;
     colorschemes.catppuccin.enable = true;
     plugins.lightline.enable = true;
+    plugins.telescope.enable = true;
     plugins.lazy.enable = true;
     plugins.dashboard = {
       enable = true;
