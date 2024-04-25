73bb8e2d9d408448de3df3fe22de43988c1d93e4
eveeifyeve <eveeg1971@gmail.com>
2024-04-25 18:09:56 +1000

    feat: theme and plugin manger

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   programs.nixvim = {
     enable = true;
-    extraPlugins = with pkgs.vimPlugins; [ lazy-nvim ];
-    colorschemes.gruvbox.enable = true;
+    colorschemes.catppuccin.enable = true;
     plugins.lightline.enable = true;
+    plugins.lazy.enable = true;
   };
 }
