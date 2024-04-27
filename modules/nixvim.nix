aff2087c1c9f446733206280d6d359822beb5f65
eveeifyeve <eveeg1971@gmail.com>
2024-04-27 15:21:32 +1000

    fix: repeating plugins

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -3,23 +3,25 @@
   programs.nixvim = {
     enable = true;
     colorschemes.catppuccin.enable = true;
-    plugins.lightline.enable = true;
-    plugins.telescope.enable = true;
-    plugins.lazy.enable = true;
-    plugins.dashboard = {
-      enable = true;
-      center = [
-        {
-          desc = "Find project";
-          icon = "📁";
-          command = "Telescope find_files";
-        }
-        {
-          desc = "Comfiguration";
-          icon = "⚙️";
-          command = "Telescope find_files ";
-        }
-      ];
+    plugins = {
+      lightline.enable = true;
+      telescope.enable = true;
+      lazy.enable = true;
+      dashboard = {
+        enable = true;
+        center = [
+          {
+            desc = "Find project";
+            icon = "📁";
+            command = "Telescope find_files";
+          }
+          {
+            desc = "Comfiguration";
+            icon = "⚙️";
+            command = "Telescope find_files ";
+          }
+        ];
+      };
       hideStatusline = true;
       hideTabline = true;
     };
