0cde7909fdba4c63fab41e3e10d5b1b960175b79
eveeifyeve <eveeg1971@gmail.com>
2024-05-12 21:13:35 +1000

    feat: neovim config update

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -4,12 +4,12 @@
     enable = true;
     colorschemes.catppuccin.enable = true;
     plugins = {
-      lightline.enable = true;
-      telescope.enable = true;
-      lazy.enable = true;
-      direnv.enable = true;
-      neocord.enable = true;
+      nvim-autopairs.enable = true;
+      luasnip.enable = true;
       treesitter.enable = true;
+
+      trouble.enable = true;
+      bufferline.enable = true;
       dashboard = {
         enable = true;
         center = [
@@ -19,7 +19,7 @@
             action = "Telescope find_files";
           }
           {
-            desc = "Comfiguration";
+            desc = "Configuration";
             icon = "⚙️";
             action = "Telescope find_files ";
           }
@@ -27,6 +27,80 @@
         hideStatusline = true;
         hideTabline = true;
       };
+
+      auto-save = {
+        enable = true;
+        enableAutoSave = true;
+      };
+      lightline.enable = true;
+      telescope.enable = true;
+
+      lazy.enable = true;
+      direnv.enable = true;
+      neocord.enable = true;
+
+      lsp = {
+        enable = true;
+        servers = {
+          rust-analyzer = {
+            enable = true;
+            autostart = true;
+            installCargo = false;
+            installRustc = false;
+          };
+          kotlin-language-server = {
+            enable = true;
+            autostart = true;
+          };
+          nil_ls = {
+            enable = true;
+            autostart = true;
+          };
+          java-language-server = {
+            enable = true;
+            autostart = true;
+          };
+          biome = {
+            enable = true;
+            autostart = true;
+          };
+          astro = {
+            enable = true;
+            autostart = true;
+          };
+          tsserver = {
+            enable = true;
+            autostart = true;
+          };
+          html = {
+            enable = true;
+            autostart = true;
+          };
+        };
+      };
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+      };
+
+      ccc.enable = true;
+      codeium-nvim.enable = true;
+      comment.enable = true;
+
+      project-nvim.enable = true;
+      todo-comments.enable = true;
+      barbar = {
+        enable = true;
+        autoHide = true;
+      };
     };
+extraConfigLua = ''
+  vim.api.nvim_exec([[
+    augroup DashboardOnVimEnter
+      autocmd!
+      autocmd VimEnter * Dashboard
+    augroup END
+  ]], false)
+'';
   };
 }
