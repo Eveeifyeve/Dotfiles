136665fc89186a2d7a831de7cd70126a075534c8
eveeifyeve <eveeg1971@gmail.com>
2024-07-03 07:11:49 +1000

    fix: rust dev enviroment

--- flakes/rust/flake.nix
+++ flakes/rust/flake.nix
@@ -2,24 +2,40 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions"; # VSCode Instance
-    fenix.url = "github:nix-community/fenix";
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      nix-vscode-extensions,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ inputs.devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
-
       perSystem =
         {
+          config,
           self',
           inputs',
           lib,
           pkgs,
+          system,
           ...
         }:
+        let
+          extensions = inputs.nix-vscode-extensions.extensions.${system};
+          inherit (pkgs) vscode-with-extensions vscodium;
+        in
         {
           devenv.shells.default = {
             difftastic.enable = true;
@@ -31,12 +47,15 @@
                   SystemConfiguration
                 ]
               )
-              && [
-                pkgs.vscode-with-extensions.override
-                {
-                  vscode = pkgs.vscodium;
-                  vscodeExtensions = with pkgs.extensions; [ vscode-marketplace.rust-lang.rust-analyzer vscode-marketplace.dustypomerleau.rust-syntax vscode-marketplace.serayuzgur.crates ];
-                }
+              ++ [
+                (vscode-with-extensions.override {
+                  vscode = vscodium;
+                  vscodeExtensions = with extensions; [
+                    vscode-marketplace.rust-lang.rust-analyzer
+                    vscode-marketplace.dustypomerleau.rust-syntax
+                    vscode-marketplace.serayuzgur.crates
+                  ];
+                })
               ];
 
             enterShell = ''
