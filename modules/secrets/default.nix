3053fa03d42183ece73c516156705db7e39e18e1
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-11 01:57:59 +1000

    chore: cleaned up secrets folder

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,6 +1,3 @@
-let 
-  user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook";
-in 
 {
-  "secrets1.age".publicKeys = [ user1]
+  "secrets1.age".publicKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"];
 }
