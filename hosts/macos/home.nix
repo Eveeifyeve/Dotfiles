5b3bf158290690789494f3dc8a183b064356d5a3
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-14 12:37:21 +1000

    fix: xcode to be latest

--- hosts/macos/home.nix
+++ hosts/macos/home.nix
@@ -19,7 +19,7 @@
       raycast # MacOS Spotlight Alternative
       iterm2 # MacOS Terminal
       utm # MacOS Qemu
-      darwin.xcode
+      darwin.xcode_15_1
     ]);
     sessionPath = [
       "$HOME/.local/bin"
