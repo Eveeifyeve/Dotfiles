71111be0828bf649ca55c4ec0264879104563037
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-18 14:43:03 +1000

    feat: apc extension testing

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,8 +1,37 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
+let
+    apc-extension = pkgs.fetchFromGitHub {
+      owner = "drcika";
+      repo = "apc-extension";
+      rev = "d4cc908bf2869fe354aa0c103bab063aa09fd491"; 
+      hash = "sha256-RfzaP+a7ukLCqL2+Ty6xFGnmgkc8lcpd1G2Xy4sr8IE=";
+    }; 
+in
 with pkgs; 
 [
       # Development Tools 
       vscode
+      # (vscode.overrideAttrs (attrs: {
+      #   postInstall = ''
+      #     cd $out
+      #     mkdir apc-extension
+
+      #     sed '1d' ${apc-extension}/src/patch.ts >> $out/apc-extension/patch.ts
+      #     sed "s%require.main!.filename%'$out/lib/vscode/resources/app/out/dummy'%g" -i  $out/apc-extension/patch.ts
+      #     sed "s%vscode.window.showErrorMessage(%throw new Error(installationPath + %g" -i  $out/apc-extension/patch.ts
+      #     sed "s%promptRestart();%%g" -i  $out/apc-extension/patch.ts
+
+      #     sed '1d' ${apc-extension}/src/utils.ts > $out/apc-extension/utils.ts
+      #     ls $out/apc-extension >> log
+
+      #     echo "import { install } from './patch.ts'; install({ extensionPath: '${apc-extension}' })" > $out/apc-extension/install.ts
+
+      #     bun apc-extension/install.ts
+      #   '';
+      #   buildInputs = attrs.buildInputs ++ [
+      #     pkgs.bun
+      #   ];
+      # }))
       devenv
       gradle
       btop
