7abf69d08c4cb31cddcf21fab523cb00ef0fd9dc
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-26 19:03:45 +1000

    feat: spotify downloader

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,75 +1,77 @@
-{pkgs, lib, ...}:
+{ pkgs, lib, ... }:
 let
-    apc-extension = pkgs.fetchFromGitHub {
-      owner = "drcika";
-      repo = "apc-extension";
-      rev = "d4cc908bf2869fe354aa0c103bab063aa09fd491"; 
-      hash = "sha256-RfzaP+a7ukLCqL2+Ty6xFGnmgkc8lcpd1G2Xy4sr8IE=";
-    }; 
+  apc-extension = pkgs.fetchFromGitHub {
+    owner = "drcika";
+    repo = "apc-extension";
+    rev = "d4cc908bf2869fe354aa0c103bab063aa09fd491";
+    hash = "sha256-RfzaP+a7ukLCqL2+Ty6xFGnmgkc8lcpd1G2Xy4sr8IE=";
+  };
 in
-with pkgs; 
+with pkgs;
 [
-      # Development Tools 
-      vscode
-      # (vscode.overrideAttrs (attrs: {
-      #   postInstall = ''
-      #     cd $out
-      #     mkdir apc-extension
+  # Development Tools 
+  vscode
+  # (vscode.overrideAttrs (attrs: {
+  #   postInstall = ''
+  #     cd $out
+  #     mkdir apc-extension
 
-      #     sed '1d' ${apc-extension}/src/patch.ts >> $out/apc-extension/patch.ts
-      #     sed "s%require.main!.filename%'$out/lib/vscode/resources/app/out/dummy'%g" -i  $out/apc-extension/patch.ts
-      #     sed "s%vscode.window.showErrorMessage(%throw new Error(installationPath + %g" -i  $out/apc-extension/patch.ts
-      #     sed "s%promptRestart();%%g" -i  $out/apc-extension/patch.ts
+  #     sed '1d' ${apc-extension}/src/patch.ts >> $out/apc-extension/patch.ts
+  #     sed "s%require.main!.filename%'$out/lib/vscode/resources/app/out/dummy'%g" -i  $out/apc-extension/patch.ts
+  #     sed "s%vscode.window.showErrorMessage(%throw new Error(installationPath + %g" -i  $out/apc-extension/patch.ts
+  #     sed "s%promptRestart();%%g" -i  $out/apc-extension/patch.ts
 
-      #     sed '1d' ${apc-extension}/src/utils.ts > $out/apc-extension/utils.ts
-      #     ls $out/apc-extension >> log
+  #     sed '1d' ${apc-extension}/src/utils.ts > $out/apc-extension/utils.ts
+  #     ls $out/apc-extension >> log
 
-      #     echo "import { install } from './patch.ts'; install({ extensionPath: '${apc-extension}' })" > $out/apc-extension/install.ts
+  #     echo "import { install } from './patch.ts'; install({ extensionPath: '${apc-extension}' })" > $out/apc-extension/install.ts
 
-      #     bun apc-extension/install.ts
-      #   '';
-      #   buildInputs = attrs.buildInputs ++ [
-      #     pkgs.bun
-      #   ];
-      # }))
-      devenv
-      gradle
-      btop
-      ripgrep
-      jd-gui
-      tree # File Sizes
+  #     bun apc-extension/install.ts
+  #   '';
+  #   buildInputs = attrs.buildInputs ++ [
+  #     pkgs.bun
+  #   ];
+  # }))
+  devenv
+  gradle
+  btop
+  ripgrep
+  jd-gui
+  tree # File Sizes
 
-      # Command Line Proccesors 
-      eza
-      jaq # Faster version of jq
-      jq # Sometimes jaq doesn't work so I use jq
-      bat # Better cat
-      ripgrep
-      gnused
-      gawk
-      fzf # Fuzzy Finder
+  # Command Line Proccesors 
+  eza
+  jaq # Faster version of jq
+  jq # Sometimes jaq doesn't work so I use jq
+  bat # Better cat
+  ripgrep
+  gnused
+  gawk
+  fzf # Fuzzy Finder
 
-      # Programs
-      spotify
-      discord
-      audacity
+  # Programs
+  spotify
+  discord
+  audacity
 
-      # Nix Tools
-      nixd
-      nix-output-monitor
+  # Nix Tools
+  nixd
+  nix-output-monitor
 
+  # Private Browsing / DarkWeb Browsers
+  tor
 
-      # Private Browsing / DarkWeb Browsers
-      tor
+  # Minecraft
+  prismlauncher-unwrapped
 
-      # Minecraft
-      prismlauncher-unwrapped
+  # Video/Photo/Graphic Editingc
+  gimp
+  # blender
+  ffmpeg_7-full
 
-      # Video/Photo/Graphic Editingc
-      gimp
-      # blender
-      ffmpeg_7-full
+  # Music Downloaders
+  spotdl
 
-      # Fonts
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    ]
+  # Fonts
+  (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+]
