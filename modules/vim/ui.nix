6fc5c714d409b6dc272630d46331e371b32796e6
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-07 12:21:13 +1000

    chore: fix plugins in ui nix

--- modules/vim/ui.nix
+++ modules/vim/ui.nix
@@ -1,22 +1,24 @@
-{...}: {
+{ ... }:
+{
   programs.nixvim = {
+    plugins = {
+      # File finder
+      telescope.enable = true;
 
-    # File finder
-    telescope.enable = true;
+      # Line
+      lualine = {
+        enable = true;
+      };
 
-    # Line
-    lualine = {
-      enable = true;
-    };
+      # Code issues displayed clearly 
+      trouble.enable = true;
 
-    # Code issues displayed clearly 
-    trouble.enable = true;
-
-    # Top files
-    barbar = {
-      enable = true;
-      settings = {
-        auto_hide = false;
+      # Top files
+      barbar = {
+        enable = true;
+        settings = {
+          auto_hide = false;
+        };
       };
     };
     extraConfigLua = ''
