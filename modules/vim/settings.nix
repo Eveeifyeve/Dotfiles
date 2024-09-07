92ff05f9bdf53fa25fd7b7175b1e68f9626dfbed
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-07 13:01:32 +1000

    feat: vim settings

--- modules/vim/settings.nix
+++ modules/vim/settings.nix
@@ -8,9 +8,23 @@
 
     # Options for nvim
     opts = {
+
+      # Numbers
       number = true;
       relativenumber = true;
+
+      # General Settings
+      swapfile = false;
+      termguicolors = true;
       shiftwidth = 2;
+
+      # Searching 
+      hlsearch = false;
+      incsearch = true;
+    };
+
+    globals = {
+      mapleader = " ";
     };
 
     # Keymaps
