9f78e594b5dfb15fa493035d99e5bcf758ff832f
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-11 18:21:47 +1000

    feat: useful nvim settings

--- modules/vim/settings.nix
+++ modules/vim/settings.nix
@@ -16,7 +16,15 @@
       # General Settings
       swapfile = false;
       termguicolors = true;
+      showmode = false; # Cause I have a status line.
+      wrap = false; # Wrapping that makes code a pain to know what line it is on.
+      smartcase = true; # Ignore what case in search and just search the thing.
+
+      # Indenting 
       shiftwidth = 2;
+      smartindent = true; 
+      tabstop = 2;
+
 
       # Searching 
       hlsearch = false;
