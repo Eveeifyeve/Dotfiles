70da530602f640c6748249704de595d15a848d0d
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-13 23:48:24 +1000

    feat: which-key and noice with notify

--- modules/vim/ui.nix
+++ modules/vim/ui.nix
@@ -1,74 +1,85 @@
 { ... }:
 {
-  programs.nixvim = {
-    plugins = {
+	programs.nixvim = {
+		plugins = {
 # File finder
-      telescope = {
-	enable = true;
-	extensions = {
-	  file-browser = {
-	    enable = true;
-	    settings = {
-	      file_browser = {
-		hijack_netrw = true;
-	      };
-	    };
-	  };
-	  fzf-native.enable = true;
-	};
+			telescope = {
+				enable = true;
+				extensions = {
+					file-browser = {
+						enable = true;
+						settings = {
+							hijack_netrw = true;
+						};
+					};
+					fzf-native.enable = true;
+				};
 
-	keymaps = {
-	  "<leader><space>" = {
-	    action = "find_files";
-	    options = {
-	      desc = "Find project files";
-	    };
-	  };
-	  "<leader>fb" = {
-	    action = "file_browser";
-	    options = {
-	      desc = "Browse Project";
-	    };
-	  };
-	  "<leader>:" = {
-	    action = "command_history";
-	    options = {
-	      desc = "Command history";
-	    };
-	  };
-	  "<leader>gf" = {
-	    action = "git_files";
-	    options = {
-	      desc = "Search git files";
-	    };
-	  };
-	  "<leader>gc" = {
-	    action = "git_commits";
-	    options = {
-	      desc = "Commits";
-	    };
-	  };
-	  "<leader>ft" = {
-	    action = "live_grep";
-	    options = {
-	      desc = "Find text";
-	    };
-	  };
-	};
-      };
+				keymaps = {
+					"<leader><space>" = {
+						action = "find_files";
+						options = {
+							desc = "Find project files";
+						};
+					};
+					"<leader>fb" = {
+						action = "file_browser";
+						options = {
+							desc = "Browse Project";
+						};
+					};
+					"<leader>:" = {
+						action = "command_history";
+						options = {
+							desc = "Command history";
+						};
+					};
+					"<leader>gf" = {
+						action = "git_files";
+						options = {
+							desc = "Search git files";
+						};
+					};
+					"<leader>gc" = {
+						action = "git_commits";
+						options = {
+							desc = "Commits";
+						};
+					};
+					"<leader>ft" = {
+						action = "live_grep";
+						options = {
+							desc = "Find text";
+						};
+					};
+				};
+			};
 
 # Line
-      lualine = {
-	enable = true;
-      };
+			lualine = {
+				enable = true;
+			};
 
 # Code issues displayed clearly 
-      trouble.enable = true;
+			trouble.enable = true;
 
-      harpoon = {
-	enable = true;
-	enableTelescope = true;
-      };
-    };
-  };
+			which-key = {
+				enable = true;
+				settings.notify = true;
+			};
+
+			noice = {
+				enable = true;
+				notify.enabled = true;
+				messages.enabled = true;
+			};
+
+			notify.enable = true;
+
+			harpoon = {
+				enable = true;
+				enableTelescope = true;
+			};
+		};
+	};
 }
