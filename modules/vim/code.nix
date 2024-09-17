f0c2077b73a79d021cb378f73b7432779dd21425
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-17 21:45:04 +1000

    fix: indenting

--- modules/vim/code.nix
+++ modules/vim/code.nix
@@ -1,140 +1,140 @@
 { ... }:
 {
-  programs.nixvim.plugins = {
+	programs.nixvim.plugins = {
 
-    #  Luasnip
-    luasnip.enable = true;
-    friendly-snippets.enable = true;
+#  Luasnip
+		luasnip.enable = true;
+		friendly-snippets.enable = true;
 
-    # Cmp
-    cmp-nvim-lsp.enable = true;
-    cmp_luasnip.enable = true;
-    dap.enable = true;
-    cmp-dap.enable = true;
-    cmp = {
-      enable = true;
-      autoEnableSources = true;
-      settings = {
-        performance = {
-          debounce = 60;
-          fetchingTimeout = 200;
-          maxViewEntries = 30;
-        };
-        snippet = {
-          expand = "luasnip";
-        };
-        sources = [
-          { name = "nvim_lsp"; }
-          {
-            name = "luasnip"; # snippets
-            keywordLength = 3;
-          }
-          { name = "cmp-dap"; }
-        ];
-      };
-    };
+# Cmp
+		cmp-nvim-lsp.enable = true;
+		cmp_luasnip.enable = true;
+		dap.enable = true;
+		cmp-dap.enable = true;
+		cmp = {
+			enable = true;
+			autoEnableSources = true;
+			settings = {
+				performance = {
+					debounce = 60;
+					fetchingTimeout = 200;
+					maxViewEntries = 30;
+				};
+				snippet = {
+					expand = "luasnip";
+				};
+				sources = [
+				{ name = "nvim_lsp"; }
+				{
+					name = "luasnip"; # snippets
+						keywordLength = 3;
+				}
+				{ name = "cmp-dap"; }
+				];
+			};
+		};
 
-    # Saving 
-    auto-save = {
-      enable = true;
-      settings.enable = true;
-    };
+# Saving 
+		auto-save = {
+			enable = true;
+			settings.enable = true;
+		};
 
-    # Commenting 
-    comment.enable = true;
+# Commenting 
+		comment.enable = true;
 
-    # Highlighting
-    treesitter.enable = true;
+# Highlighting
+		treesitter.enable = true;
 
-    # Formatter plugins
-    conform-nvim = {
-      enable = true;
-      settings.formatters_by_ft = {
-        javascript = [ "dprint" ];
-        typescript = [ "dprint" ];
-        astro = [ "dprint" ];
-      };
-    };
+# Formatter plugins
+		conform-nvim = {
+			enable = true;
+			settings.formatters_by_ft = {
+				javascript = [ "dprint" ];
+				typescript = [ "dprint" ];
+				astro = [ "dprint" ];
+			};
+		};
 
-    # LSP Stuff
-    lsp = {
-      enable = true;
-      servers = {
-        nil-ls = {
-          enable = true;
-          autostart = true;
-        };
+# LSP Stuff
+		lsp = {
+			enable = true;
+			servers = {
+				nil-ls = {
+					enable = true;
+					autostart = true;
+				};
 
-				# Markdown Lsp
+# Markdown Lsp
 				marksman = {
 					enable = true;
 					autostart = true;
 				};
 
 
-	# Web Dev
-        biome = {
-          enable = true;
-          autostart = true;
-        };
-        astro = {
-          enable = true;
-          autostart = true;
-        };
+# Web Dev
+				biome = {
+					enable = true;
+					autostart = true;
+				};
+				astro = {
+					enable = true;
+					autostart = true;
+				};
 				tailwindcss = {
 					enable = true;
 					autostart = true;
 				}
-        tsserver = {
-          enable = true;
-          autostart = true;
-        };
-        html = {
-          enable = true;
-          autostart = true;
-        };
+				tsserver = {
+					enable = true;
+					autostart = true;
+				};
+				html = {
+					enable = true;
+					autostart = true;
+				};
 
 
-	# System programing
-        rust-analyzer = {
-          enable = true;
-          autostart = true;
-          installRustc = false;
-          installCargo = false;
-        };
+# System programing
+				rust-analyzer = {
+					enable = true;
+					autostart = true;
+					installRustc = false;
+					installCargo = false;
+				};
 
-	zls = {
-	  enable = true;
-	  autostart = true;
+				zls = {
+					enable = true;
+					autostart = true;
+				};
+
+				gopls = {
+					enable = true;
+					autostart = true;
+				};
+
+# Python 
+				ruff = {
+					enable = true;
+					autostart = true;
+				};
+				pyright = {
+					enable = true; 
+					autostart = true;
+				};
+
+# Minecraft Development 
+
+				java-language-server = {
+					enable = true;
+					autostart = true;
+				};
+
+				kotlin-language-server = {
+					enable = true;
+					autostart = true;
+				};
+			};
+		};
 	};
-
-	gopls = {
-	  enable = true;
-	  autostart = true;
-	};
-
-	# Python 
-	ruff = {
-	  enable = true;
-	  autostart = true;
-	};
-	pyright = {
-	  enable = true; 
-	  autostart = true;
-	};
-
-	# Minecraft Development 
-
-	java-language-server = {
-	  enable = true;
-	  autostart = true;
-	};
-
-	kotlin-language-server = {
-	  enable = true;
-	  autostart = true;
-	};
-      };
-    };
-  };
 }
