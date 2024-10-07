2cc1ef69fe453416caca8e8bfce0710ff6fbe699
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-07 06:15:05 +0000

    feat: vim & eveeifyeve home user

--- hosts/eveeifyeve/configuration.nix
+++ hosts/eveeifyeve/configuration.nix
@@ -16,8 +16,17 @@
 
   environment.systemPackages = [
     pkgs.curl
+    pkgs.vim
     pkgs.gitMinimal
   ];
+  
+  users.users.eveeifyeve = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    hashedPassword = "$y$j9T$1FNXj4e3flI0uJ.paVPFq/$2T/1y0PVRB0Le1h0UAg4pUNyWHG8Kyy0la9sNm86arC";
+    extraGroups = ["nixos-config"];
+  };
+  programs.zsh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"
