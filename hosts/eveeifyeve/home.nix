76c6a454186b29416ba5329bf3228ce64d8ce284
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-07 22:21:00 +0000

    feat: hyprland & kitty

--- hosts/eveeifyeve/home.nix
+++ hosts/eveeifyeve/home.nix
@@ -12,14 +12,32 @@
 	wayland.windowManager.hyprland = {
 		enable = true;
 		xwayland.enable = true;
-		systemd.enable = false;
+		systemd = {
+			enable = false;
+			enableXdgAutostart = true;
+		};
+		settings = {
+			"$mod" = "SUPER";
+			bindm = [
+				"$mod, T, exec kitty"
+			] ++ (
+				builtins.concatLists (builtins.genList (i: 
+					let ws = i + 1;
+					in [
+						"$mod, code:1${toString i}, workspace, ${toString ws}"
+						"$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+					]
+				)
+				9)
+			);
+		};
 	};
 	home = {
 		username = "eveeifyeve";
 		stateVersion = "24.05";
 		packages = pkgs.callPackage ../../modules/packages.nix { } 
 		++ (with pkgs; [
-			
+			kitty
 		]);
 		shellAliases.nix-rebuild = "sudo nixos-rebuild switch --flake /root/.dotfiles .#eveeifyeve --json |& nom --json";
 	};
