b3adafc946b60abbbd2c3c184b416e59dd242c91
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-08 01:05:10 +0000

    fix: hm by backing up files

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
 					 	home-manager.nixosModules.home-manager {
 					 	  home-manager.useGlobalPkgs = true;
 					 	  home-manager.useUserPackages = true;
+							home-manager.backupFileExtension = "backup";
 					 	  home-manager.users.eveeifyeve = import ./hosts/eveeifyeve/home.nix;
 					 	}
 					];
