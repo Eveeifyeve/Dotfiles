8ef9253c04c1e6f6e676c25081a8b7b481613d11
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-09 15:21:02 +0000

    feat: basic hyprconf

--- hosts/eveeifyeve/home.nix
+++ hosts/eveeifyeve/home.nix
@@ -15,9 +15,29 @@
 		xwayland.enable = true;
 		systemd.enable = false;
 		settings = {
+			decoration = {
+				rounding = 10;
+				active_opacity = 1.0;
+				inactive_opacity = 1.0;
+
+				drop_shadow = true;
+				shadow_range = 4;
+				shadow_render_power = 3;
+
+				blur = {
+					enabled = true;
+					size = 3;
+					passes = 1; 
+					vibrancy = 0.1696;
+				};
+			};
+			animation = {
+				enabled = true;
+			};
 			"$mod" = "SUPER";
 			bind = [
 				"$mod, T, exec, kitty"
+				"$mod, C, killactive"
 			] ++ (
 				builtins.concatLists (builtins.genList (i: 
 					let ws = i + 1;
