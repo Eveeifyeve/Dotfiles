98817cf4fbe3854c937b38a1e88261f6822cba0f
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-10 03:42:42 +0000

    feat: pipewire

--- hosts/eveeifyeve/configuration.nix
+++ hosts/eveeifyeve/configuration.nix
@@ -28,6 +28,14 @@ in
 
 	hardware.enableRedistributableFirmware = true;
 
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
+
 	hardware.graphics = {
 		package = hypr-unstable-pkgs.mesa.drivers;
 		enable = true;
