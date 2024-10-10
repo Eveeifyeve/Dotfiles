379d26a3528ece7d3f600e30fc303487b6f425ca
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-10 22:34:08 +1100

    fix: discord screenshare

--- hosts/eveeifyeve/configuration.nix
+++ hosts/eveeifyeve/configuration.nix
@@ -57,6 +57,23 @@ in
 		enable32Bit = true;	
 	};
 
+
+	xdg.portal = {
+		enable = true;
+		wlr.enable = true;
+
+		extraPortals = with pkgs; [
+			xdg-desktop-portal-gtk
+			xdg-desktop-portal
+		];
+
+		configPackages = with pkgs; [
+			xdg-desktop-portal-gtk
+			xdg-desktop-portal-hyprland
+			xdg-desktop-portal
+		];
+	};
+
 	programs.hyprland = {
 		enable = true;
 		package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
