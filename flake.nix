e7de466a5f05dd7c81e957853206db05fbacf130
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-12 22:34:47 +1100

    feat: allsystems formatting and devshell

--- flake.nix
+++ flake.nix
@@ -5,25 +5,25 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-		disko = {
-			url = "github:nix-community/disko";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
 
-		# HyprLand 
-		hyprland.url  = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    # HyprLand 
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
-		hyprland-plugins = {
-			url = "github:hyprwm/hyprland-plugins";
-			inputs.hyprland.follows = "hyprland";
-		};
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
 
     # Eveeifyeve Usefull Resources.
     curseforge-nix = {
@@ -68,49 +68,61 @@
   };
 
   outputs =
-    inputs@{
-      self,
-      agenix,
-      home-manager,
-      nix-homebrew,
-      nixvim,
-      nixpkgs,
-			disko,
-      ...
+    inputs@{ self
+    , agenix
+    , home-manager
+    , nix-homebrew
+    , nixvim
+    , nixpkgs
+    , disko
+    , ...
     }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
     {
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
 
-			# NixOS
-			nixosConfigurations = {
-				eveeifyeve = nixpkgs.lib.nixosSystem {
-					system = "x86_64-linux";
-					specialArgs = {
-						inherit inputs;
-					};
-					modules = [
-						disko.nixosModules.disko
-						nixvim.nixosModules.nixvim
-						./hosts/eveeifyeve/configuration.nix
-						./modules/vim/default.nix
-					 	home-manager.nixosModules.home-manager {
-							home-manager = {
-								extraSpecialArgs = {
-									git = {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      });
+
+      # NixOS
+      nixosConfigurations = {
+        eveeifyeve = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            disko.nixosModules.disko
+            nixvim.nixosModules.nixvim
+            ./hosts/eveeifyeve/configuration.nix
+            ./modules/vim/default.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                extraSpecialArgs = {
+                  git = {
                     username = "eveeifyeve";
                     email = "88671402+Eveeifyeve@users.noreply.github.com";
                   };
-									inherit inputs;
-								};
-								useGlobalPkgs = true;
-					 	  	useUserPackages = true;
-								backupFileExtension = "backup";
-					 	  	users.eveeifyeve = import ./hosts/eveeifyeve/home.nix;
-							};
-					 	}
-					];
-				};
-			};
+                  inherit inputs;
+                };
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                users.eveeifyeve = import ./hosts/eveeifyeve/home.nix;
+              };
+            }
+          ];
+        };
+      };
 
       # Nix on Darwin with Nix-Darwin x HM
       darwinConfigurations = {
@@ -131,7 +143,7 @@
                     username = "eveeifyeve";
                     email = "88671402+Eveeifyeve@users.noreply.github.com";
                   };
-									inherit inputs;
+                  inherit inputs;
                 };
                 useGlobalPkgs = true;
                 useUserPackages = true;
@@ -144,6 +156,13 @@
           ];
         };
       };
+
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      });
 
       # Nix Flake Templates for Shell Environments
       # https://github.com/Eveeifyeve/flake-templates
