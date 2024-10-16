84e6c333e9379d05bcbce3dce636d8fa0cd60bf0
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-17 07:38:54 +1100

    chore: moved hypride to home manager

--- hosts/eveeifyeve/home.nix
+++ hosts/eveeifyeve/home.nix
@@ -155,6 +155,33 @@ in
     };
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "hyprlock";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        {
+          timeout = 600;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 900;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
   home.pointerCursor = {
     gtk.enable = true;
     package = pkgs.bibata-cursors;
