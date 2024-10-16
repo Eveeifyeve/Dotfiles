d2a4e972fe0436320737d003b25de5bbbad8cb94
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-17 07:33:04 +1100

    feat: hypridle and hyprlock

--- hosts/eveeifyeve/default.nix
+++ hosts/eveeifyeve/default.nix
@@ -18,6 +18,32 @@
 
   services = {
     openssh.enable = true;
+    hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "hyprlock";
+          before_sleep_cmd = "hyprlock";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          {
+            timeout = 600;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 900;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
   };
 
   # Time
--- hosts/eveeifyeve/home.nix
+++ hosts/eveeifyeve/home.nix
@@ -121,6 +121,37 @@ in
     };
     hyprlock = {
       enable = true;
+      settings = {
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+        general = {
+          disable_loading_bar = true;
+          grace = 300;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(202, 211, 245)";
+            inner_color = "rgb(91, 96, 120)";
+            outer_color = "rgb(24, 25, 38)";
+            outline_thickness = 5;
+            placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
+            shadow_passes = 2;
+          }
+        ];
+      };
     };
   };
 
