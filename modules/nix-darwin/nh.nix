9c6d93cd7107725ddf30d04e45066701cfb82bd8
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-28 06:38:06 +1100

    fix: nh_darwin user clean option

--- modules/nix-darwin/nh.nix
+++ modules/nix-darwin/nh.nix
@@ -9,7 +9,6 @@
     enable = true;
     clean = {
       enable = false; # TODO: Change this when you get rid of the gc.
-      user = config.home.username;
     };
     package = inputs.nh_darwin.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
