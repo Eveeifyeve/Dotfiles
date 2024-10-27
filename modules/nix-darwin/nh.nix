d0085f8a77ddf21780f0784bb7bc3870eb7fb3da
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-28 06:45:01 +1100

    chore: cleaned up nh_darwin fork

--- modules/nix-darwin/nh.nix
+++ modules/nix-darwin/nh.nix
@@ -7,9 +7,7 @@
 {
   programs.nh = {
     enable = true;
-    clean = {
-      enable = false; # TODO: Change this when you get rid of the gc.
-    };
-    package = inputs.nh_darwin.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    clean.enable = false; # TODO: Change this when you get rid of the gc.
+    # package = inputs.nh_darwin.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 }
