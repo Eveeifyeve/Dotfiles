fb2530368b5f5e6f1d4968b33efbc75ec403fb73
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-11-26 16:43:19 +1100

    feat: homebrew module nix-homebrew

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -1,16 +1,67 @@
-{ inputs, ... }:
+{ lib, ... }:
+let
+  inherit (lib) types;
+  cfg = config.homebrew;
+in
 {
-  nix-homebrew = {
-    user = "eveeifyeve";
-    enable = true;
-    enableRosetta = true;
-    taps = {
-      "homebrew/homebrew-core" = inputs.homebrew-core;
-      "homebrew/homebrew-cask" = inputs.homebrew-cask;
-      "homebrew/bundle" = inputs.homebrew-bundle;
-      "homebrew/homebrew-cask-versions" = inputs.homebrew-cask-versions;
+  options.homebrew = {
+    user = lib.mkOption {
+      type = types.str;
+      default = "eveeifyeve";
     };
-    mutableTaps = false;
-    autoMigrate = false;
+    install = lib.mkOption {
+      type = types.bool;
+      default = true;
+    };
+    rosetta = lib.mkOption {
+      type = types.bool;
+      default = false;
+    };
+    taps = {
+      type = types.attrsOf types.package;
+      default = {
+        "homebrew/homebrew-core" = inputs.homebrew-core;
+        "homebrew/homebrew-cask" = inputs.homebrew-cask;
+        "homebrew/bundle" = inputs.homebrew-bundle;
+        "homebrew/homebrew-cask-versions" = inputs.homebrew-cask-versions;
+      };
+    };
+    mutableTaps = lib.mkOption {
+      type = types.bool;
+      default = false;
+    };
+    autoMigrate = lib.mkOption {
+      type = types.str;
+      default = "eveeifyeve";
+    };
+  };
+
+  config = {
+    homebrew = {
+      enable = true;
+      casks = [
+        "homebrew/cask/docker"
+        "cloudflare-warp"
+        "logitech-g-hub"
+      ];
+      brews = [
+        "brightness" # Adjust Screen Brightness on MacOS using CLI
+      ];
+      masApps = {
+        GarageBand = 682658836;
+        TestFlight = 899247664;
+        CrystalFetch = 6454431289;
+      };
+      onActivation.cleanup = "uninstall";
+    };
+
+    nix-homebrew.darwinModules.nix-homebrew.nix-homebrew = {
+      user = cfg.user;
+      enable = cfg.install;
+      enableRosetta = cfg.rosetta;
+      taps = cfg.taps;
+      mutableTaps = cfg.mutableTaps;
+      autoMigrate = cfg.autoMigrate;
+    };
   };
 }
