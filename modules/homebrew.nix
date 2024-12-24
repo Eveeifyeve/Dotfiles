0bd345153d08dcf7e62d563bff8db70fcf2c847f
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-12-24 20:21:40 +1100

    feat: homebrew module

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -1,16 +1,90 @@
-{ inputs, ... }:
+{ lib, config, ... }:
+let
+  inherit (lib) types;
+  cfg = config.homebrew;
+in
 {
-  nix-homebrew = {
-    user = "eveeifyeve";
-    enable = true;
-    enableRosetta = true;
-    taps = {
-      "homebrew/homebrew-core" = inputs.homebrew-core;
-      "homebrew/homebrew-cask" = inputs.homebrew-cask;
-      "homebrew/bundle" = inputs.homebrew-bundle;
-      "homebrew/homebrew-cask-versions" = inputs.homebrew-cask-versions;
+  options.homebrew = {
+    casks = lib.mkOption {
+      types = types.listOf types.str;
+      deafult = [];
     };
-    mutableTaps = false;
-    autoMigrate = false;
+    brews = lib.mkOption {
+      types = types.listOf types.str;
+      deafult = [];
+    };
+
+    masApps = {
+      types = types.attrsOf types.ints.positive;
+      default = {};
+    };
+    activation = {
+      cleanup = lib.mkOption {
+        types = types.enum [ "none" "uninstall" "zap" ];
+        deafult = "uninstall";
+      };
+    };
+
+    nix-homebrew = {
+      enable = lib.mkOption {
+        type = types.bool;
+        default = true;
+      };
+      user = lib.mkOption {
+        type = types.nullOr types.str;
+        default = cfg.system.username;
+      };
+      taps = {
+        type = types.attrsOf types.package;
+        default = {
+          "homebrew/homebrew-core" = inputs.homebrew-core;
+          "homebrew/homebrew-cask" = inputs.homebrew-cask;
+          "homebrew/bundle" = inputs.homebrew-bundle;
+          "homebrew/homebrew-cask-versions" = inputs.homebrew-cask-versions;
+      };
+      rosetta = lib.mkOption {
+        type = types.bool;
+        default = false;
+      };
+      mutableTaps = lib.mkOption {
+        type = types.bool;
+        default = false;
+      };
+      autoMigrate = lib.mkOption {
+        type = types.str;
+        default = "eveeifyeve";
+      };
+    };
+    
+  };
+};
+
+  config = {
+    homebrew = {
+      enable = true;
+      casks = cfg.casks
+        # "homebrew/cask/docker"
+        # "cloudflare-warp"
+        # "logitech-g-hub"
+      brews = cfg.brews;
+        # "brightness" # Adjust Screen Brightness on MacOS using CLI
+      masApps = cfg.masApps
+        # GarageBand = 682658836;
+        # TestFlight = 899247664;
+        # CrystalFetch = 6454431289;
+      };
+      onActivation.cleanup = cfg.activation.cleanup;
+    };
+
+    nix-homebrew.darwinModules.nix-homebrew.nix-homebrew = let
+      nix-homebrew = cfg.nix-homebrew;
+    in {
+      user = nix-homebrew.user;
+      enable = nix-homebrew.install;
+      enableRosetta = nix-homebrew.rosetta;
+      taps = nix-homebrew.taps;
+      mutableTaps = nix-homebrew.mutableTaps;
+      autoMigrate = nix-homebrew.autoMigrate;
+    };
   };
 }
