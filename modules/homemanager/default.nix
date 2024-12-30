01c82ff5c5568f79d785bd67ed4250a94d04c1f5
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-12-30 20:36:56 +1100

    chore: remove posix shellAliases
    todo: nushell aliases

--- modules/homemanager/default.nix
+++ modules/homemanager/default.nix
@@ -15,17 +15,17 @@
     ssh.enable = true;
   };
   home.shellAliases = {
-    proc = "ps u | head -n1 && ps aux | rg -v '\\srg\\s-\\.' | rg";
-    gitr = ''
-      	gitr () {
-      		for f in $(find . -type d -name .git | awk -F"/.git$" '{print $1}');  do
-      			echo
-      			echo "................................ (cd $f && git $*) ........................................."
-      			echo
-      			(cd $f && git $*)
-      		done
-      }
-    '';
+    # proc = "ps u | head -n1 && ps aux | rg -v '\\srg\\s-\\.' | rg";
+		#  gitr = ''
+		#    	gitr () {
+		#    		for f in $(find . -type d -name .git | awk -F"/.git$" '{print $1}');  do
+		#    			echo
+		#    			echo "................................ (cd $f && git $*) ........................................."
+		#    			echo
+		#    			(cd $f && git $*)
+		#    		done
+		#    }
+		#  '';
     cat = "bat";
   };
   nix.settings = {
