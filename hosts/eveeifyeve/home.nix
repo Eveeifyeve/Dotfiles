acd1cb0d29d7154ec1fbc8a685cb5a005f9f2b42
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-01-28 12:41:32 +1100

    fix: hypridle settings

--- hosts/eveeifyeve/home.nix
+++ hosts/eveeifyeve/home.nix
@@ -149,23 +149,23 @@ in
       enable = true;
       settings = {
         general = {
-          lock_cmd = "hyprlock";
-          before_sleep_cmd = "hyprlock";
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
           after_sleep_cmd = "hyprctl dispatch dpms on";
         };
 
         listener = [
           {
-            timeout = 600;
-            on-timeout = "hyprlock";
+            timeout = 1800;
+            on-timeout = "loginctl lock-session";
           }
           {
-            timeout = 900;
+            timeout = 1810;
             on-timeout = "hyprctl dispatch dpms off";
             on-resume = "hyprctl dispatch dpms on";
           }
           {
-            timeout = 1800;
+            timeout = 3600;
             on-timeout = "systemctl suspend";
           }
         ];
