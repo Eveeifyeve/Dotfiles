bbfb29e84e17f9b31a65a10b00cefa7389ee2813
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-02-08 17:08:28 +1100

    feat: working java lsp

--- modules/vim/code.nix
+++ modules/vim/code.nix
@@ -106,93 +106,10 @@
 
       nvim-jdtls = {
         enable = true;
-        data.__raw = "vim.fn.stdpath 'cache' .. '/jdtls/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t')";
-        configuration.__raw = ''vim.fn.stdpath 'cache' .. "/jdtls/config"'';
+				data = "~/.cache/jdtls/workspace";
+				configuration = "~/.cache/jdtls/configurations";
         initOptions = {
           bundles.__raw = "_M.jdtls.bundles";
-        };
-        settings = {
-          java = {
-            configuration = {
-              updateBuildConfiguration = "interactive";
-              runtimes = [
-                pkgs.zulu23
-                pkgs.zulu17
-                pkgs.zulu11
-                pkgs.zulu8
-                pkgs.zulu
-              ];
-            };
-            completion = {
-              favoriteStaticMembers = [ ];
-              filteredTypes = [
-                "com.sun.*"
-                "io.micrometer.shaded.*"
-                "java.awt.*"
-                "jdk.*"
-                "sun.*"
-              ];
-              importOrder = [
-                "java"
-                "javax"
-                "com"
-                "org"
-              ];
-            };
-          };
-          eclipse = {
-            downloadSources = true;
-          };
-          format = {
-            enabled = true;
-            settings = {
-              url = "${
-                (pkgs.fetchurl {
-                  url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml";
-                  sha256 = "sha256-51Uku2fj/8iNXGgO11JU4HLj28y7kcSgxwjc+r8r35E=";
-                })
-              }";
-              profile = "GoogleStyle";
-            };
-          };
-          implementationCodeLens = {
-            enabled = true;
-          };
-          import = {
-            gradle = {
-              enabled = true;
-              wrapper = {
-                enabled = true;
-              };
-            };
-            maven = {
-              enabled = true;
-            };
-          };
-          inlayHints = {
-            parameterNames = {
-              enabled = "all";
-            };
-          };
-          maven = {
-            downloadSources = true;
-          };
-          references = {
-            includeDecompiledSources = true;
-          };
-          referencesCodeLens = {
-            enabled = true;
-          };
-          signatureHelp = {
-            enabled = true;
-          };
-          preferred = "fernflower";
-          sources = {
-            organizeImports = {
-              starThreshold = 9999;
-              staticStarThreshold = 9999;
-            };
-          };
         };
       };
 
