f1faebb944b05df1fd6b97c86346c67de7e89b41
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-03-08 23:33:38 +1100

    feat: zoxide

--- modules/homemanager/default.nix
+++ modules/homemanager/default.nix
@@ -12,27 +12,33 @@
       nix-direnv.enable = true;
     };
     gpg.enable = true;
-		password-store = {
-			enable = true;
-			settings = {
-				PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
-				PASSWORD_STORE_KEY = "EBA9DF00EE9717990BC39BDCBAA8C2C616D55AB3";
-			};
-		};
+    password-store = {
+      enable = true;
+      settings = {
+        PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+        PASSWORD_STORE_KEY = "EBA9DF00EE9717990BC39BDCBAA8C2C616D55AB3";
+      };
+    };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
+      options = [ "--cmd cd" ];
+    };
     ssh.enable = true;
   };
   home.shellAliases = {
     # proc = "ps u | head -n1 && ps aux | rg -v '\\srg\\s-\\.' | rg";
-		#  gitr = ''
-		#    	gitr () {
-		#    		for f in $(find . -type d -name .git | awk -F"/.git$" '{print $1}');  do
-		#    			echo
-		#    			echo "................................ (cd $f && git $*) ........................................."
-		#    			echo
-		#    			(cd $f && git $*)
-		#    		done
-		#    }
-		#  '';
+    #  gitr = ''
+    #    	gitr () {
+    #    		for f in $(find . -type d -name .git | awk -F"/.git$" '{print $1}');  do
+    #    			echo
+    #    			echo "................................ (cd $f && git $*) ........................................."
+    #    			echo
+    #    			(cd $f && git $*)
+    #    		done
+    #    }
+    #  '';
     cat = "bat";
   };
   nix.settings = {
