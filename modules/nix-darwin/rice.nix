21e24053a89bd3481431337c9b1edb8647f97874
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-04-29 22:01:20 +1000

    chore: added more ricing

--- modules/nix-darwin/rice.nix
+++ modules/nix-darwin/rice.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   inherit (config.lib.stylix) colors;
 in
@@ -6,17 +6,27 @@ in
   services.jankyborders = {
     enable = true;
     style = "round";
-    width = 5.0;
     hidpi = true;
   };
 
-  services.sketchybar = {
-    enable = true;
-    config = ''
-      sketchybar --bar position=top height=40 blur_radius=30 color=0x${colors.base00}
+  services.sketchybar =
+    let
+      clock = pkgs.writeShellBinScript ''
+        sketchybar --set "clock" label=\"$(date '+%H   :    %M')\"
+      '';
+    in
+    {
+      enable = true;
+      config = ''
+              sketchybar --bar position=left topmost="window" height=42 margin=10 padding_left=4 padding_right=4 corner_radius=10 y_offset=8 border_width=1 border_color=${config.services.jankyborders.active_color}
+        			sketchybar --add event aerospace_workspace_change
 
+              sketchybar --update
+      '';
+    };
 
-    '';
-  };
+  system.activationScripts.postUserActivation.text = ''
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"${config.stylix.image}\""
+  '';
 
 }
