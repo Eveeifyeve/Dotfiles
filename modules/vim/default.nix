4a9cd783ca3b33f314997735a3d97e04f0f6661d
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-05-12 17:54:36 +1000

    chore: remove config not needed

--- modules/vim/default.nix
+++ modules/vim/default.nix
@@ -16,33 +16,6 @@
       ccc.enable = true;
       todo-comments.enable = true;
     };
-
-    extraPlugins = [
-      (pkgs.vimPlugins.cord-nvim)
-    ];
-    extraConfigLua = ''
-      require("cord").setup({
-      	display = {
-      	  show_time = true,
-      	  swap_fields = false,
-      	  swap_icons = false,
-      	},
-      	ide = {
-      	  enable = true,
-      	  show_status = true,
-      	  timeout = 300000,
-      	  text = 'Idle',
-      	  tooltip = '💤',
-      	},
-      	text = {
-      	  viewing = 'Viewing {}',                    	 
-      	  editing = 'Editing {}',                    
-      	  file_browser = 'Browsing files in {}',     	  
-      	  vcs = 'Committing changes in {}',
-      	  workspace = 'In {}', 
-      	  },
-      })
-    '';
   };
   imports = [
     ./settings.nix
