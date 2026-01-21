962c501b7cd10e4d0d2f280d454b5d8bf7fb2ce4
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2026-01-21 23:56:33 +1100

    feat: eye-candy fastfetch startup when terminal starts up

--- modules/homemanager/terminal.nix
+++ modules/homemanager/terminal.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs = {
     zsh = {
@@ -8,12 +8,14 @@
         plugins = [ "vi-mode" ];
       };
       initContent = ''
-        	        # Ensure Nix is sourced. Necessary when /etc/zshrc file loses this same code block on macOS upgrades
-                        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-                          . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-        		. /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-                            fi
-        	'';
+                	        # Ensure Nix is sourced. Necessary when /etc/zshrc file loses this same code block on macOS upgrades
+                                if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+                                  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+                		. /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+                                    fi
+
+        				${lib.getExe pkgs.fastfetch}
+                	'';
     };
     nushell = {
       enable = true;
