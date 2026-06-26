f1e1a3a4e5771c4e341d721bb5adb3ffc2936b95
eveeifyeve <kbkk7250@outlook.com>
2026-06-26 14:25:42 +1000

    feat(nixvim): dap-ui

--- modules/programs/editors/nixvim/plugins/dap.nix
+++ modules/programs/editors/nixvim/plugins/dap.nix
@@ -4,6 +4,34 @@
       dap.enable = true;
       dap-lldb.enable = true;
       cmp-dap.enable = true;
+      dap-ui = {
+        enable = true;
+        lazyLoad = {
+          enable = true;
+          settings = {
+            before.__raw = ''
+              function()
+                require('lz.n').trigger_load('nvim-dap')
+              end
+            '';
+            keys = [
+              {
+                __unkeyed-1 = "<leader>du";
+                __unkeyed-2.__raw = ''
+                  								function()
+                  								require('dap.ext.vscode').load_launchjs(nil, {
+                  									["java"] = {"java"}
+                  								})
+
+                  								require("dapui").toggle()
+                  							end
+                '';
+                desc = "Toggle Debugger UI";
+              }
+            ];
+          };
+        };
+      };
     };
   };
 }
