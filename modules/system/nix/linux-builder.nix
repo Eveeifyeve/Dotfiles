929296a581a92fb06a70914542347ac666609459
eveeifyeve <kbkk7250@outlook.com>
2026-06-30 00:05:26 +1000

    fix(darwin): linux builder

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -1,15 +1,26 @@
 { lib, ... }:
+
+let
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+in
 {
   darwin.modules.base =
     { pkgs, ... }:
     {
+      nix.settings.extra-platforms = systems;
       nix.linux-builder = {
         enable = false;
+        inherit systems;
         package = pkgs.darwin.linux-builder;
         ephemeral = true;
         mandatoryFeatures = [ "nixos-test" ];
         config = {
-          boot.binfmt.emulatedSystems = [ "x86_64-linux" ]; # Allows to emulated for building for more archtechures
+          boot.binfmt.emulatedSystems = builtins.filter (
+            system: system != "aarch64-linux" && lib.strings.hasSuffix "-linux" system
+          ) systems; # Allows to emulated for building for more archtechures
           nix.settings.experimental-features = "nix-command flakes";
           security.sudo.wheelNeedsPassword = false; # Allows the ability to ssh into the builder for any reason.
           users.users."builder".extraGroups = [ "wheel" ];
