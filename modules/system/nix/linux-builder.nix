b109570993aedb31d361b8f1a5b090cfb9137c6e
eveeifyeve <kbkk7250@outlook.com>
2026-07-03 15:30:21 +1000

    chore(linux-builder): remove package

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -7,26 +7,23 @@ let
   ];
 in
 {
-  darwin.modules.base =
-    { pkgs, ... }:
-    {
-      nix.settings.extra-platforms = systems;
-      nix.linux-builder = {
-        enable = false;
-        inherit systems;
-        package = pkgs.darwin.linux-builder;
-        ephemeral = true;
-        mandatoryFeatures = [ "nixos-test" ];
-        config = {
-          boot.binfmt.emulatedSystems = builtins.filter (
-            system: system != "aarch64-linux" && lib.strings.hasSuffix "-linux" system
-          ) systems; # Allows to emulated for building for more archtechures
-          nix.settings.experimental-features = "nix-command flakes";
-          security.sudo.wheelNeedsPassword = false; # Allows the ability to ssh into the builder for any reason.
-          users.users."builder".extraGroups = [ "wheel" ];
-          virtualisation.diskSize = lib.mkForce (1024 * 100);
-          virtualisation.cores = 5;
-        };
+  darwin.modules.base = {
+    nix.settings.extra-platforms = systems;
+    nix.linux-builder = {
+      enable = false;
+      inherit systems;
+      ephemeral = true;
+      mandatoryFeatures = [ "nixos-test" ];
+      config = {
+        boot.binfmt.emulatedSystems = builtins.filter (
+          system: system != "aarch64-linux" && lib.strings.hasSuffix "-linux" system
+        ) systems; # Allows to emulated for building for more archtechures
+        nix.settings.experimental-features = "nix-command flakes";
+        security.sudo.wheelNeedsPassword = false; # Allows the ability to ssh into the builder for any reason.
+        users.users."builder".extraGroups = [ "wheel" ];
+        virtualisation.diskSize = lib.mkForce (1024 * 100);
+        virtualisation.cores = 5;
       };
     };
+  };
 }
