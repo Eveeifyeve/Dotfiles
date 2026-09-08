07bc182b1dc5c70f9fc999fa3d449b3947434f8a
eveeifyeve <kbkk7250@outlook.com>
2026-09-08 14:32:52 +1000

    fix: z-image and deepseek flash model config

--- modules/eveeifyeve/ai.nix
+++ modules/eveeifyeve/ai.nix
@@ -3,8 +3,8 @@
     sops.secrets.openrouter-apikey-eveeifyeve.sopsFile = ./secrets/openrouter-apikey;
 
     programs.opencode.settings = {
-      model = "~z-ai/glm-flash-latest";
-      small_model = "~deepseek/deepseek-v4-flash-latest";
+      model = "openrouter/~z-ai/glm-flash-latest";
+      small_model = "openrouter/~deepseek/deepseek-v4-flash-latest";
       provider.openrouter.options.apiKey = "{file:${hmArgs.config.sops.secrets.openrouter-apikey-eveeifyeve.path}}";
     };
   };
