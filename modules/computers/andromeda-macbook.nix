31d5f2ff7270c3e663228a32b66312ab3b86f7f0
eveeifyeve <kbkk7250@outlook.com>
2026-09-12 01:59:57 +1000

    chore(andromeda): add openlogi as hm

--- modules/computers/andromeda-macbook.nix
+++ modules/computers/andromeda-macbook.nix
@@ -1,16 +1,20 @@
 { config, ... }:
 {
-  darwin.configurations.andromeda-macbook.module = {
-    services.nix-daemon.logFile = "/var/log/nix-daemon.log";
-    nixpkgs.hostPlatform.system = "aarch64-darwin";
-    nix.settings.trusted-users = [
-      "root"
-      "@admin"
-    ];
-    system.stateVersion = 7;
-    imports = with config.darwin.modules; [
-      base
-      gui
-    ];
-  };
+  darwin.configurations.andromeda-macbook.module =
+    { pkgs, ... }:
+    {
+      services.nix-daemon.logFile = "/var/log/nix-daemon.log";
+      nixpkgs.hostPlatform.system = "aarch64-darwin";
+      nix.settings.trusted-users = [
+        "root"
+        "@admin"
+      ];
+      system.stateVersion = 7;
+      imports = with config.darwin.modules; [
+        base
+        gui
+      ];
+
+      home-manager.users.eveeifyeve.home.packages = [ pkgs.openlogi ];
+    };
 }
