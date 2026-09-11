43cbe8a8afa94d9b0178fb111f25b46e1707d8f2
eveeifyeve <kbkk7250@outlook.com>
2026-09-12 01:02:49 +1000

    chore(darwin): switch back to apple virt linux-builder

--- modules/system/nix/linux-builder.nix
+++ modules/system/nix/linux-builder.nix
@@ -4,13 +4,8 @@
     {
       nix.linux-builder = {
         enable = true;
-        package = pkgs.darwin.linux-builder;
+        package = pkgs.darwin.linux-builder-vz;
         ephemeral = true;
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-        config.boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
       };
     };
 }
